c361f2171c6340f8e1cecee038db092072e220b3
Enrique <enrique,magdalenoc@gmail.com>
2024-06-30 14:11:19 -0600

    feature:
     * First version of code-history app
     * Added gitignore file

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,20 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .font(.footnote)
-                .fontWeight(.heavy)
-                .foregroundColor(Color.purple)
-                .multilineTextAlignment(.leading)
-            Text("Hello world, again!")
-                .font(.title)
-        }
-        .padding()
+        ZStack{
+            mainColor.ignoresSafeArea()
+            VStack{
+                Text("1/10")
+                    .font(.callout)
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack{
+                    Button(
+                        action: {
+                            print("Tapped on choice 1")
+                        },
+                        label: {
+                            Text("Ant")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor,width: 4)
+                        }
+                    )
+                    Button(
+                        action: {
+                            print("Tapped on choice 2")
+                        },
+                        label: {
+                            Text("Beetle")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor,width: 4)
+                        }
+                    )
+                    Button(
+                        action: {
+                            print("Tapped on choice 3")
+                        },
+                        label: {
+                            Text("Moth")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor,width: 4)
+                        }
+                    )
+                    Button(
+                        action: {
+                            print("Tapped on choice 4")
+                        },
+                        label: {
+                            Text("Fly")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor,width: 4)
+                        }
+                    )
+                }
+            }
+        }.foregroundColor(.white)
     }
 }
 
